d31c08f10def689cca658b49996f1559c1bcec62
Oleg Rytov <cattimofei@gmail.com>
2023-07-21 13:03:34 +0300

    edit Project

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(int weight, double height) {
         double result;
-        result = weight / (height * height);
+        result = weight / (Math.pow(height, 2));
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int m = 98;
-        double h = 1.87;
+        int m = 98; // масса тела
+        double h = 1.87; // рост в метрах
         int bmi = (int) service.calculate(m, h);
         System.out.println("Индекс массы тела = " + bmi);
     }
